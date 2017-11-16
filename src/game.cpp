df7d9c3a42b5ba0a062ecd207915d9bee06a5824
Vova Abdrakhmanov <369565@gmail.com>
2017-11-16 20:48:25 +0300

    start new prototype

---
+++ src/game.cpp
@@ -0,0 +1,37 @@
+#include <map>
+#include <string>
+#include <vector>
+
+struct vec2 {
+    float x;
+    float y;
+};
+
+class Bullet {};
+class Tank {};
+class Tile {};
+
+class BulletFactory {
+    BulletFactory( std::string config_file, std::string tiles_file ){};
+    Bullet create( std::string type, vec2 position, float speed, vec2 direction );
+};
+
+class TankFactory {
+    TankFactory( std::string config_file, std::string tiles_file ){};
+    Tank create( std::string type, vec2 position, vec2 direction );
+};
+
+class TileFactory {
+    TileFactory( std::string config_file, std::string tiles_file ){};
+    Tile create( std::string type, vec2 position );
+};
+
+class Game {
+    std::vector<Bullet> bullets;
+    std::vector<Tank> tanks;
+    std::map<std::pair<int, int>, Tile> tiles;
+    int height;
+    int width;
+    void next();
+    Game(){};
+};
